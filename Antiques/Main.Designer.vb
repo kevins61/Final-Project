a24646eaaa00e170014d3447efe7ffe6f4415487
Kevin Sandberg <kevins61@hotmail.com>
2016-04-04 21:25:06 -0400

    Updated for Check 1

--- Antiques/Main.Designer.vb
+++ Antiques/Main.Designer.vb
@@ -24,11 +24,11 @@ Partial Class Main
     Private Sub InitializeComponent()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.tsmFile = New System.Windows.Forms.ToolStripMenuItem()
-        Me.tsmSaveClose = New System.Windows.Forms.ToolStripMenuItem()
-        Me.tsmExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tsmFileSaveClose = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tsmFileExit = New System.Windows.Forms.ToolStripMenuItem()
         Me.tsmEdit = New System.Windows.Forms.ToolStripMenuItem()
-        Me.tsmEditAuctions = New System.Windows.Forms.ToolStripMenuItem()
-        Me.tsmEditInventory = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tsmPuchaseLots = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tsmEditInventoryItems = New System.Windows.Forms.ToolStripMenuItem()
         Me.tsmReports = New System.Windows.Forms.ToolStripMenuItem()
         Me.tsmReportsPurchaseActivity = New System.Windows.Forms.ToolStripMenuItem()
         Me.tsmReportsShowInventory = New System.Windows.Forms.ToolStripMenuItem()
@@ -38,10 +38,18 @@ Partial Class Main
         Me.lblStatus = New System.Windows.Forms.ToolStripStatusLabel()
         Me.cboSelectAuction = New System.Windows.Forms.ComboBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnAddAuction = New System.Windows.Forms.Button()
         Me.btnExit = New System.Windows.Forms.Button()
         Me.txtDate = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.lstAuctionList = New System.Windows.Forms.ListBox()
+        Me.txtAuctionCo = New System.Windows.Forms.TextBox()
+        Me.txtAmount = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnShowAll = New System.Windows.Forms.Button()
+        Me.tsmEditRecordSales = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tsmReportsSales = New System.Windows.Forms.ToolStripMenuItem()
         Me.MenuStrip1.SuspendLayout()
         Me.StatusStrip1.SuspendLayout()
         Me.SuspendLayout()
@@ -52,51 +60,51 @@ Partial Class Main
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsmFile, Me.tsmEdit, Me.tsmReports, Me.tsmHelp})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(637, 28)
+        Me.MenuStrip1.Size = New System.Drawing.Size(590, 28)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
         '
         'tsmFile
         '
-        Me.tsmFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsmSaveClose, Me.tsmExit})
+        Me.tsmFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsmFileSaveClose, Me.tsmFileExit})
         Me.tsmFile.Name = "tsmFile"
         Me.tsmFile.Size = New System.Drawing.Size(44, 24)
         Me.tsmFile.Text = "&File"
         '
-        'tsmSaveClose
+        'tsmFileSaveClose
         '
-        Me.tsmSaveClose.Name = "tsmSaveClose"
-        Me.tsmSaveClose.Size = New System.Drawing.Size(171, 26)
-        Me.tsmSaveClose.Text = "&Save && Close"
+        Me.tsmFileSaveClose.Name = "tsmFileSaveClose"
+        Me.tsmFileSaveClose.Size = New System.Drawing.Size(181, 26)
+        Me.tsmFileSaveClose.Text = "&Save && Close"
         '
-        'tsmExit
+        'tsmFileExit
         '
-        Me.tsmExit.Name = "tsmExit"
-        Me.tsmExit.Size = New System.Drawing.Size(171, 26)
-        Me.tsmExit.Text = "E&xit"
+        Me.tsmFileExit.Name = "tsmFileExit"
+        Me.tsmFileExit.Size = New System.Drawing.Size(181, 26)
+        Me.tsmFileExit.Text = "E&xit"
         '
         'tsmEdit
         '
-        Me.tsmEdit.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsmEditAuctions, Me.tsmEditInventory})
+        Me.tsmEdit.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsmPuchaseLots, Me.tsmEditInventoryItems, Me.tsmEditRecordSales})
         Me.tsmEdit.Name = "tsmEdit"
         Me.tsmEdit.Size = New System.Drawing.Size(47, 24)
         Me.tsmEdit.Text = "&Edit"
         '
-        'tsmEditAuctions
+        'tsmPuchaseLots
         '
-        Me.tsmEditAuctions.Name = "tsmEditAuctions"
-        Me.tsmEditAuctions.Size = New System.Drawing.Size(145, 26)
-        Me.tsmEditAuctions.Text = "A&uctions"
+        Me.tsmPuchaseLots.Name = "tsmPuchaseLots"
+        Me.tsmPuchaseLots.Size = New System.Drawing.Size(185, 26)
+        Me.tsmPuchaseLots.Text = "&Puchase Lots"
         '
-        'tsmEditInventory
+        'tsmEditInventoryItems
         '
-        Me.tsmEditInventory.Name = "tsmEditInventory"
-        Me.tsmEditInventory.Size = New System.Drawing.Size(145, 26)
-        Me.tsmEditInventory.Text = "&Inventory"
+        Me.tsmEditInventoryItems.Name = "tsmEditInventoryItems"
+        Me.tsmEditInventoryItems.Size = New System.Drawing.Size(185, 26)
+        Me.tsmEditInventoryItems.Text = "&Inventory Items"
         '
         'tsmReports
         '
-        Me.tsmReports.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsmReportsPurchaseActivity, Me.tsmReportsShowInventory})
+        Me.tsmReports.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsmReportsPurchaseActivity, Me.tsmReportsShowInventory, Me.tsmReportsSales})
         Me.tsmReports.Name = "tsmReports"
         Me.tsmReports.Size = New System.Drawing.Size(72, 24)
         Me.tsmReports.Text = "&Reports"
@@ -111,7 +119,7 @@ Partial Class Main
         '
         Me.tsmReportsShowInventory.Name = "tsmReportsShowInventory"
         Me.tsmReportsShowInventory.Size = New System.Drawing.Size(195, 26)
-        Me.tsmReportsShowInventory.Text = "&Show Inventory"
+        Me.tsmReportsShowInventory.Text = "Show &Inventory"
         '
         'tsmHelp
         '
@@ -123,7 +131,7 @@ Partial Class Main
         'tsmHelpAbout
         '
         Me.tsmHelpAbout.Name = "tsmHelpAbout"
-        Me.tsmHelpAbout.Size = New System.Drawing.Size(125, 26)
+        Me.tsmHelpAbout.Size = New System.Drawing.Size(181, 26)
         Me.tsmHelpAbout.Text = "&About"
         '
         'StatusStrip1
@@ -132,7 +140,7 @@ Partial Class Main
         Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.lblStatus})
         Me.StatusStrip1.Location = New System.Drawing.Point(0, 467)
         Me.StatusStrip1.Name = "StatusStrip1"
-        Me.StatusStrip1.Size = New System.Drawing.Size(637, 25)
+        Me.StatusStrip1.Size = New System.Drawing.Size(590, 25)
         Me.StatusStrip1.TabIndex = 1
         Me.StatusStrip1.Text = "StatusStrip1"
         '
@@ -145,11 +153,10 @@ Partial Class Main
         'cboSelectAuction
         '
         Me.cboSelectAuction.FormattingEnabled = True
-        Me.cboSelectAuction.Items.AddRange(New Object() {"1000", "1001", "1002"})
         Me.cboSelectAuction.Location = New System.Drawing.Point(137, 76)
         Me.cboSelectAuction.Name = "cboSelectAuction"
         Me.cboSelectAuction.Size = New System.Drawing.Size(121, 24)
-        Me.cboSelectAuction.TabIndex = 0
+        Me.cboSelectAuction.TabIndex = 5
         '
         'Label1
         '
@@ -160,53 +167,121 @@ Partial Class Main
         Me.Label1.TabIndex = 3
         Me.Label1.Text = "Select Auction:"
         '
-        'btnUpdate
+        'btnAddAuction
         '
-        Me.btnUpdate.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.btnUpdate.Location = New System.Drawing.Point(99, 366)
-        Me.btnUpdate.Name = "btnUpdate"
-        Me.btnUpdate.Size = New System.Drawing.Size(118, 35)
-        Me.btnUpdate.TabIndex = 2
-        Me.btnUpdate.Text = "Update"
-        Me.btnUpdate.UseVisualStyleBackColor = True
+        Me.btnAddAuction.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnAddAuction.Location = New System.Drawing.Point(93, 410)
+        Me.btnAddAuction.Name = "btnAddAuction"
+        Me.btnAddAuction.Size = New System.Drawing.Size(118, 35)
+        Me.btnAddAuction.TabIndex = 3
+        Me.btnAddAuction.Text = "Add Auction"
+        Me.btnAddAuction.UseVisualStyleBackColor = True
         '
         'btnExit
         '
         Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.btnExit.Location = New System.Drawing.Point(355, 366)
+        Me.btnExit.Location = New System.Drawing.Point(358, 410)
         Me.btnExit.Name = "btnExit"
         Me.btnExit.Size = New System.Drawing.Size(102, 35)
-        Me.btnExit.TabIndex = 3
+        Me.btnExit.TabIndex = 4
         Me.btnExit.Text = "E&xit"
         Me.btnExit.UseVisualStyleBackColor = True
         '
         'txtDate
         '
-        Me.txtDate.Location = New System.Drawing.Point(417, 78)
+        Me.txtDate.Location = New System.Drawing.Point(86, 360)
         Me.txtDate.Name = "txtDate"
         Me.txtDate.Size = New System.Drawing.Size(100, 22)
-        Me.txtDate.TabIndex = 1
+        Me.txtDate.TabIndex = 0
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(369, 82)
+        Me.Label2.Location = New System.Drawing.Point(89, 339)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(42, 17)
         Me.Label2.TabIndex = 9
         Me.Label2.Text = "Date:"
         '
+        'lstAuctionList
+        '
+        Me.lstAuctionList.FormattingEnabled = True
+        Me.lstAuctionList.ItemHeight = 16
+        Me.lstAuctionList.Location = New System.Drawing.Point(41, 148)
+        Me.lstAuctionList.Name = "lstAuctionList"
+        Me.lstAuctionList.Size = New System.Drawing.Size(493, 180)
+        Me.lstAuctionList.TabIndex = 10
+        '
+        'txtAuctionCo
+        '
+        Me.txtAuctionCo.Location = New System.Drawing.Point(220, 359)
+        Me.txtAuctionCo.Name = "txtAuctionCo"
+        Me.txtAuctionCo.Size = New System.Drawing.Size(143, 22)
+        Me.txtAuctionCo.TabIndex = 1
+        '
+        'txtAmount
+        '
+        Me.txtAmount.Location = New System.Drawing.Point(425, 361)
+        Me.txtAmount.Name = "txtAmount"
+        Me.txtAmount.Size = New System.Drawing.Size(100, 22)
+        Me.txtAmount.TabIndex = 2
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(217, 339)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(122, 17)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "Auction Company:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(433, 339)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(60, 17)
+        Me.Label5.TabIndex = 16
+        Me.Label5.Text = "Amount:"
+        '
+        'btnShowAll
+        '
+        Me.btnShowAll.Location = New System.Drawing.Point(298, 70)
+        Me.btnShowAll.Name = "btnShowAll"
+        Me.btnShowAll.Size = New System.Drawing.Size(96, 34)
+        Me.btnShowAll.TabIndex = 6
+        Me.btnShowAll.Text = "Show All"
+        Me.btnShowAll.UseVisualStyleBackColor = True
+        '
+        'tsmEditRecordSales
+        '
+        Me.tsmEditRecordSales.Name = "tsmEditRecordSales"
+        Me.tsmEditRecordSales.Size = New System.Drawing.Size(185, 26)
+        Me.tsmEditRecordSales.Text = "&Record Sales"
+        '
+        'tsmReportsSales
+        '
+        Me.tsmReportsSales.Name = "tsmReportsSales"
+        Me.tsmReportsSales.Size = New System.Drawing.Size(195, 26)
+        Me.tsmReportsSales.Text = "&Sales"
+        '
         'Main
         '
-        Me.AcceptButton = Me.btnUpdate
+        Me.AcceptButton = Me.btnAddAuction
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.CancelButton = Me.btnExit
-        Me.ClientSize = New System.Drawing.Size(637, 492)
+        Me.ClientSize = New System.Drawing.Size(590, 492)
+        Me.Controls.Add(Me.btnShowAll)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtAmount)
+        Me.Controls.Add(Me.txtAuctionCo)
+        Me.Controls.Add(Me.lstAuctionList)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.txtDate)
         Me.Controls.Add(Me.btnExit)
-        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnAddAuction)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.cboSelectAuction)
         Me.Controls.Add(Me.StatusStrip1)
@@ -229,10 +304,10 @@ Partial Class Main
     Friend WithEvents tsmEdit As ToolStripMenuItem
     Friend WithEvents tsmReports As ToolStripMenuItem
     Friend WithEvents tsmReportsPurchaseActivity As ToolStripMenuItem
-    Friend WithEvents tsmSaveClose As ToolStripMenuItem
-    Friend WithEvents tsmExit As ToolStripMenuItem
-    Friend WithEvents tsmEditAuctions As ToolStripMenuItem
-    Friend WithEvents tsmEditInventory As ToolStripMenuItem
+    Friend WithEvents tsmFileSaveClose As ToolStripMenuItem
+    Friend WithEvents tsmFileExit As ToolStripMenuItem
+    Friend WithEvents tsmPuchaseLots As ToolStripMenuItem
+    Friend WithEvents tsmEditInventoryItems As ToolStripMenuItem
     Friend WithEvents tsmReportsShowInventory As ToolStripMenuItem
     Friend WithEvents tsmHelp As ToolStripMenuItem
     Friend WithEvents tsmHelpAbout As ToolStripMenuItem
@@ -240,8 +315,16 @@ Partial Class Main
     Friend WithEvents lblStatus As ToolStripStatusLabel
     Friend WithEvents cboSelectAuction As ComboBox
     Friend WithEvents Label1 As Label
-    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnAddAuction As Button
     Friend WithEvents btnExit As Button
     Friend WithEvents txtDate As TextBox
     Friend WithEvents Label2 As Label
+    Friend WithEvents lstAuctionList As ListBox
+    Friend WithEvents txtAuctionCo As TextBox
+    Friend WithEvents txtAmount As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents btnShowAll As Button
+    Friend WithEvents tsmEditRecordSales As ToolStripMenuItem
+    Friend WithEvents tsmReportsSales As ToolStripMenuItem
 End Class
--- Antiques/Main.vb
+++ Antiques/Main.vb
@@ -6,7 +6,12 @@
 '****************************************************************************************
 
 Public Class Main
-    Dim theDate As Date
+    Dim theDate As Date = Today
+    Dim amount As Decimal = 0
+    Dim auctioneer As String = ""
+    Dim lastID As Integer = 0
+    Dim count As Integer = 0
+
     Dim myAuctionsArray() As String = {"1000,1/23/15,Joe Blow Auctions,115.40", "1001,4/3/15,George's Auctions,105.76", "1002,11/4/15,Auction House,93.25"}
 
     ''' <summary>
@@ -14,6 +19,14 @@ Public Class Main
     ''' </summary>
     Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         lblStatus.Text = ""
+
+        For Each a In myAuctionsArray
+            Dim fields() As String = a.Split(","c)
+            cboSelectAuction.Items.Add(fields(0).Trim())
+            lastID = CInt(fields(0))
+            count += 1
+            lstAuctionList.Items.Add(fields(0) & ", " & CDate(fields(1)) & ", " & fields(2) & ", " & CDec(fields(3)).ToString("c"))
+        Next
     End Sub
 
     ''' <summary>
@@ -31,26 +44,68 @@ Public Class Main
         frmAbout.ShowDialog()
     End Sub
 
-    Private Sub tsmExit_Click(sender As Object, e As EventArgs) Handles tsmExit.Click
+    Private Sub tsmExit_Click(sender As Object, e As EventArgs) Handles tsmFileExit.Click
         Me.Close()
     End Sub
     '********End of code for all menu items **********
 
     Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboSelectAuction.SelectedIndexChanged
+        If cboSelectAuction.SelectedIndex > -1 Then
+            lstAuctionList.Items.Clear()
+            For Each a In myAuctionsArray
+                Dim field() As String = a.Split(","c)
+                If (field(0) = cboSelectAuction.SelectedItem.ToString) Then
+                    lstAuctionList.Items.Add(field(0) & ", " & CDate(field(1)) & ", " & field(2) & ", " & CDec(field(3)).ToString("c"))
+                    Return
+                End If
+            Next
+        End If
+    End Sub
+
+    Sub updateDisplay()
+        lstAuctionList.Items.Clear()
+
+        For Each a In myAuctionsArray
+            Dim field() As String = a.Split(","c)
+            lstAuctionList.Items.Add(field(0) & ", " & CDate(field(1)) & ", " & field(2) & ", " & CDec(field(3)).ToString("c"))
+        Next
+    End Sub
+    Private Sub btnAddAuction_Click(sender As Object, e As EventArgs) Handles btnAddAuction.Click
+        cboSelectAuction.SelectedIndex = -1
+        If Not (Date.TryParse(txtDate.Text, theDate)) Then
+            UpdateStatus("Oh-oh, you entered an invalid date! Try again.")
+            txtDate.Focus()
+            Return
+        End If
+        If (txtAuctionCo.Text = "") Then
+            UpdateStatus("Please enter an auction company")
+            txtAuctionCo.Focus()
+            Return
+        End If
+        If Not (Decimal.TryParse(txtAmount.Text, amount)) Then
+            UpdateStatus("Please enter a valid amount")
+            txtAmount.Focus()
+            Return
+        End If
+        lastID += 1
+        ReDim Preserve myAuctionsArray(count)
+        myAuctionsArray(count) = lastID.ToString & ", " & theDate.ToString & ", " & txtAuctionCo.Text & ", " & amount.ToString
+        cboSelectAuction.Items.Add(lastID)
+        count += 1
+        updateDisplay()
+        txtDate.Text = ""
+        txtAuctionCo.Text = ""
+        txtAmount.Text = ""
+        UpdateStatus("Record added successfully as Auction ID " & lastID)
 
     End Sub
 
-    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
-        txtDate.BackColor = DefaultBackColor
-        txtDate.ForeColor = DefaultForeColor
-        If (Date.TryParse(txtDate.Text, theDate)) Then
-            lblStatus.Text = "Good Job - Valid Date"
-        Else
-            lblStatus.Text = "Oh-oh, you entered an invalid date! Try again."
-            lblStatus.ForeColor = Color.Red
-            txtDate.Focus()
-            txtDate.BackColor = Color.Yellow
+    Sub UpdateStatus(ByVal msg As String)
+        lblStatus.Text = msg
+    End Sub
 
-        End If
+    Private Sub btnShowAll_Click(sender As Object, e As EventArgs) Handles btnShowAll.Click
+        cboSelectAuction.SelectedIndex = -1
+        updateDisplay()
     End Sub
 End Class
